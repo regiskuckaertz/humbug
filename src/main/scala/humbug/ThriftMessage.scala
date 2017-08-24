279f3cf24c82969749d0750ef95d656541cf5f0e
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2017-08-24 07:14:07 +0100

    Abstract message type with enum

--- src/main/scala/humbug/ThriftMessage.scala
+++ src/main/scala/humbug/ThriftMessage.scala
@@ -1,41 +1,26 @@
 package humbug
 
-abstract class ThriftMessage[A](
+abstract class ThriftMessageType
+case object ThriftCall extends ThriftMessageType
+case object ThriftReply extends ThriftMessageType
+case object ThriftException extends ThriftMessageType
+case object ThriftOneWay extends ThriftMessageType
+
+class ThriftMessage[A](
   val id: Int,
+  val mtype: ThriftMessageType,
   val name: String,
   val value: A
 )
 
 object ThriftMessage {
   def apply[A](
-    mtype: Int,
+    mtype: ThriftMessageType,
     mid: Int,
     mname: String,
     mvalue: A
-  ): ThriftMessage[A] = mtype match {
-    case 1 => ThriftCall(mid, mname, mvalue)
-    case 2 => ThriftReply(mid, mname, mvalue)
-    case 3 => ThriftException(mid, mname, mvalue)
-    case 4 => ThriftOneWay(mid, mname, mvalue)
-  }
+  ): ThriftMessage[A] = new ThriftMessage(
+    mid, mtype, mname, mvalue
+  )
 }
 
-case class ThriftCall[A](
-  override val id: Int,
-  override val name: String,
-  override val value: A) extends ThriftMessage[A](id, name, value)
-
-case class ThriftReply[A](
-  override val id: Int,
-  override val name: String,
-  override val value: A) extends ThriftMessage[A](id, name, value)
-
-case class ThriftException[A](
-  override val id: Int,
-  override val name: String,
-  override val value: A) extends ThriftMessage[A](id, name, value)
-
-case class ThriftOneWay[A](
-  override val id: Int,
-  override val name: String,
-  override val value: A) extends ThriftMessage[A](id, name, value)
