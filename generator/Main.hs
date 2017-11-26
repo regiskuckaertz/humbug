70c846478e2aec9812957e10daa47bacd9d3f7f7
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2017-11-26 13:20:32 +0000

    Handle source and destination options

--- generator/Main.hs
+++ generator/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Control.Monad((>=>))
+import Control.Monad((>=>), guard)
 import Control.Monad.IO.Class
 import Humbug.Compile
 import Humbug.Print
@@ -9,7 +9,14 @@ import Humbug.Thrift
 import Humbug.Tokenize
 import Humbug.Types
 import Data.List(uncons, intersperse, isSuffixOf)
-import System.Directory(getCurrentDirectory, listDirectory)
+import System.Directory
+  ( getCurrentDirectory
+  , createDirectoryIfMissing
+  , withCurrentDirectory
+  , listDirectory
+  , makeAbsolute
+  )
+import System.Environment(getArgs)
 import qualified Data.Map.Strict as Map
 
 loadFiles :: IO [String]
@@ -19,14 +26,24 @@ loadFiles = do
   let ts = filter (isSuffixOf ".thrift") fs 
   traverse readFile ts
 
-saveFiles :: Map.Map FilePath [Stmt] -> Eval (Map.Map FilePath ())
-saveFiles m = 
+saveFiles :: FilePath -> Map.Map FilePath [Stmt] -> Eval (Map.Map FilePath ())
+saveFiles dst m = 
   let
-    save = \f stmts -> liftIO $ writeFile (f ++ ".scala") $ concat $ intersperse "\n\n" $ fmap printScala stmts
-  in Map.traverseWithKey save m
+    save = \f stmts -> liftIO $ withCurrentDirectory dst $ writeFile (f ++ ".scala") $ concat $ intersperse "\n\n" $ fmap printScala stmts
+  in do
+    Map.traverseWithKey save m
+
+run :: FilePath -> FilePath -> Eval ()
+run src dst = 
+  do
+    asrc <- liftIO $ makeAbsolute src
+    adst <- liftIO $ makeAbsolute dst
+    _ <- liftIO $ createDirectoryIfMissing True adst
+    ts <- liftIO $ withCurrentDirectory asrc loadFiles
+    traverse (tokenize >=> compile >=> saveFiles adst) ts
+    return ()
 
 --- Compiles ./*.thrift -> ./*.scala
 main = do
-  ts <- loadFiles
-  cs <- traverse (runEval . (tokenize >=> compile >=> saveFiles)) ts
-  return ()
+  (src : dst : []) <- getArgs
+  runEval $ run src dst
