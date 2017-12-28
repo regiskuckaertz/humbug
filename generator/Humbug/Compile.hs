001b82fb244c1eb9ba3a0df26e8547d1faa7b7de
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2017-12-28 11:03:51 +0000

    Compile as a paramorphism

--- generator/Humbug/Compile.hs
+++ generator/Humbug/Compile.hs
@@ -4,195 +4,127 @@ module Humbug.Compile
 ( compile
 ) where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Except
-import Control.Monad.Trans.State.Strict
-import Data.Char(toUpper)
-import Data.List(elemIndices, foldr1, intersperse)
-import qualified Data.Map.Strict as Map
+import Control.Arrow((&&&))
+import Data.Functor.Foldable(Fix(..), para, unfix)
+import Data.List(partition, unzip)
+import Data.Tuple(swap)
 import Humbug.Scala
 import Humbug.Thrift
 import Humbug.Types
-import Humbug.Utils.Map
 
-updateEnv ∷ FilePath → Name → Name → Eval ()
-updateEnv fp pkg n = do
-  env ← lift $ gets (Map.!? fp)
-  case env of
-    Just (pkg, ns) → lift $ modify $ Map.insert fp (pkg, (n : ns))
-    Nothing → lift $ modify $ Map.insert fp (pkg, [n])
+compile ∷ Thrift → [Stmt]
+compile = para compile'
 
-compile ∷ Document → Eval (Map.Map FilePath Stmt)
-compile (Document hs ds) = do  
-  _ ← liftIO $ putStrLn ("Compiling Scala...")
-  let pkg = maybe "humbug.sample" id $ foldr buildPackageName Nothing hs
-  let pkg' = packageObjectPackage pkg
-  let imps = foldMap buildImports hs
-  defs ← foldM (compile' pkg) Map.empty ds
-  cnts ← foldM (compile'' pkg) [] ds
-  let pobj = buildPackageObject pkg cnts
-  ExceptT $ return $ Right $ mappend (prelude pkg <$> defs) (prelude pkg' <$> pobj)
-  where
-    prelude pkg stmts = scalaPackage pkg [] stmts
-    packageObjectPackage pkg = case (elemIndices '.' pkg) of
-      [] → pkg
-      is → take (last is) pkg
+compile' ∷ ThriftF (Thrift, [Stmt]) → [Stmt]
+compile' (ThDocument is ds) = docs
+  where docs = fmap (buildDocument is) ds'
+        ds' = uncurry (++) . partition isConst $ ds
+        isConst d | Fix(ThConst _ _ _) <- fst d = True
+                  | otherwise                   = False
 
-compile' ∷ Name → Map.Map FilePath [Stmt] → Definition → Eval (Map.Map FilePath [Stmt])
-compile' pkg m (Typedef ft ident) = return $ m `Map.union` Map.singleton ident (buildTypedef ident ft)
-compile' pkg m (Enum ident vs) = return $ m `Map.union` Map.singleton ident (buildEnum ident vs)
-compile' pkg m (Struct ident fs) = return $ m `Map.union` Map.singleton ident (buildStruct ident fs)
-compile' pkg m (Union ident fs) = return $ m `Map.union` Map.singleton ident (buildUnion ident fs)
-compile' _ _ _ = return mempty
+compile' (ThInclude lit) = [scalaImportPlaceholder lit]
 
-compile'' ∷ Name → [Stmt] → Definition → Eval [Stmt]
-compile'' pkg cs (Const ct cn cv) = 
-  let
-    ft = Just $ buildType ct
-    fv = buildValue cv
-  in return (scalaVal cn False False ft [fv] : cs)
-compile'' _ cs _  = return cs
+compile' (ThNamespace NsJava ident) = [scalaPackage ident]
+compile' (ThNamespace NsStar ident) = [scalaPackage ident]
+compile' (ThNamespace _ _) = [scalaNoop]
 
-buildPackageName ∷ Header → Maybe String → Maybe String
-buildPackageName (Namespace NsJava ident) _ = Just ident
-buildPackageName (Namespace NsStar ident) Nothing = Just ident
-buildPackageName _ x = x
+compile' (ThConst ct cn cv) = [scalaVal cn False False ft fv]
+  where ft = Just $ buildType ct
+        fv = [buildValue cv]
 
-buildPackageObject ∷ String → [Stmt] → Map.Map FilePath [Stmt]
-buildPackageObject pkg cnts = 
-  case cnts of
-    [] → mempty
-    _ → Map.singleton "package" [(scalaPackageObject (buildPackageObjectName pkg) cnts)]
+compile' (ThTypedef ft ident) = [vc, co]
+  where ft' = buildType ft
+        v = scalaArgument "value" (Just ft') Nothing
+        vc = scalaCaseClass ident [v] ["AnyVal", "TTypeDef"]
+        co = scalaCompanionObject ident (Just e) [menc, mdec]
+        e = "TTypeDefCodec[" ++ ident ++ "," ++ ft' ++ "]"
+        menc = scalaMethod "encode" True [] Nothing [scalaField u "value" [] []]
+        mdec = scalaMethod "decode" True [] Nothing [scalaNew ident False [u] []]
+        u = scalaIdent "_"
 
-buildImports ∷ Header → [Stmt]
-buildImports (Include lit) = [scalaImportPlaceholder lit]
-buildImports _ = []
+compile' (ThEnum ident fs) = [st, co]
+  where st = scalaSealedTrait ident (Just "TEnum") cos
+        cos = fmap ((\x → scalaCaseObject x [] ident) . fst) fs
+        co = scalaCompanionObject ident cop [menc, mdec]
+        cop = Just $ "TEnumCodec[" ++ ident ++ "]"
+        menc = scalaMethod "encode" True [] Nothing encs
+        mdec = scalaMethod "decode" True [] Nothing decs
+        encs = fst $ foldr buildEncode ([], 0) fs
+        decs = fst $ foldr buildDecode ([], 0) fs
+        buildEncode (ident, Just (CvInt v)) (stmts, _) = ((scalaCase ident Nothing [scalaLiteral v]) : stmts, v + 1)
+        buildEncode (ident, _)              (stmts, v) = ((scalaCase ident Nothing [scalaLiteral v]) : stmts, v + 1)
+        buildDecode (ident, Just (CvInt v)) (stmts, _) = ((scalaCase (show v) Nothing [scalaIdent ident]) : stmts, v + 1)
+        buildDecode (ident, _)              (stmts, v) = ((scalaCase (show v) Nothing [scalaIdent ident]) : stmts, v + 1)
 
-buildTypedef ∷ Identifier → FieldType → [Stmt]
-buildTypedef ident ft = let
-  ft' = buildType ft
-  vc = scalaCaseClass ident [scalaArgument "value" (Just ft') Nothing] ["AnyVal", "TTypeDef"]
-  ex = "TTypeDefCodec[" ++ ident ++ "," ++ (buildType ft) ++ "]"
-  u = scalaIdent "_"
-  menc = scalaMethod "encode" True [] Nothing [scalaField u "value" [] []]
-  mdec = scalaMethod "decode" True [] Nothing [scalaNew ident False [u] []]
-  o = scalaCompanionObject ident (Just ex) [menc, mdec]
-  in [vc, o]
+compile' (ThStruct ident fs) = [cc, co]
+  where cc = scalaCaseClass ident (foldMap snd fs) ["TStruct"]
+        co = scalaCompanionObject ident (Just $ "TStructCodec[" ++ ident ++ "]") (ws ++ is ++ [defs, menc, mdec])
+        (ws, is) = unzip $ fmap (buildWitness &&& buildCodec) fws
+        defs = scalaVal "defaults" True False Nothing [hds]
+        hds = scalaNew "HMap[TFieldCodec]" True ds []
+        ds = foldMap buildDefaultValue fws
+        menc = scalaMethod "encode" True [] Nothing [lenc]
+        mdec = scalaMethod "decode" True [] Nothing [ldec]
+        lenc = scalaLambda [scalaArgument "x" Nothing Nothing] [henc]
+        ldec = scalaLambda [scalaArgument "m" Nothing Nothing] [fdec]
+        henc = scalaNew "HMap[TFieldCodec]" True ps []
+        fdec = scalaFor ps' [y]
+        (ps, ps') = unzip $ fmap (buildPair &&& buildFor) fws
+        y = scalaNew ident True (fmap (scalaLiteral . getIdent) fws) []
+        fws = zip freshIds . fmap (unfix . fst) $ fs
 
-buildEnum ∷ Identifier → [(Identifier, Maybe ConstValue)] → [Stmt]
-buildEnum ident fs = [buildSealedTrait, buildCompanionObject]
-  where
-    buildSealedTrait = let
-      cos = map (\x → scalaCaseObject x [] ident) $ map fst fs
-      in scalaSealedTrait ident (Just "TEnum") cos
-    buildCompanionObject = let
-      encs = fst $ foldr buildEncode ([], 0) fs
-      decs = fst $ foldr buildDecode ([], 0) fs
-      menc = scalaMethod "encode" True [] Nothing encs
-      mdec = scalaMethod "decode" True [] Nothing decs
-      p = "TEnumCodec[" ++ ident ++ "]"
-      in scalaCompanionObject ident (Just p) [menc, mdec]
-    buildEncode (ident, Just (CvInt v)) (stmts, _) = ((scalaCase ident Nothing [scalaLiteral v]) : stmts, v + 1)
-    buildEncode (ident, _) (stmts, v) = ((scalaCase ident Nothing [scalaLiteral v]) : stmts, v + 1)
-    buildDecode (ident, Just (CvInt v)) (stmts, _) = ((scalaCase (show v) Nothing [scalaIdent ident]) : stmts, v + 1)
-    buildDecode (ident, _) (stmts, v) = ((scalaCase (show v) Nothing [scalaIdent ident]) : stmts, v + 1)
+compile' (ThUnion ident fs) = [st, co]
+  where st = scalaSealedTrait ident (Just "TUnion") ccs
+        ccs = fmap cc fws
+        cc (tf, f) = scalaCaseClass (getIdent tf) f [ident]
+        co = scalaCompanionObject ident (Just $ "TUnionCodec[" ++ ident ++ "]") (ws ++ is ++ [menc, mdec])
+        (ws, is) = unzip $ fmap ((buildWitness &&& buildCodec) . fst) fws
+        menc = scalaMethod "encode" True [] Nothing enc
+        mdec = scalaMethod "decode" True [] Nothing [scalaNoop]
+        (enc, dec) = unzip $ fmap ((buildCase &&& buildGet) . fst) fws
+        fws = uncurry zip . swap . fmap (zip freshIds . fmap unfix) . swap . unzip $ fs
+        
+compile' (ThException ident fs) = [cc, co]
+  where n = ""
+        is = []
+        cc = scalaCaseClass ident (foldMap snd fs) ["TException"]
+        co = scalaNoop
 
-buildStruct ∷ Identifier → [Field] → [Stmt]
-buildStruct ident fs = let
-  cc = scalaCaseClass ident (map buildField fs) ["TStruct"]
-  co = buildCompanionObject
-  in [cc, co]
-  where
-    buildCompanionObject = let
-      vs = iterate (+1) 1
-      fids = fst $ foldr buildFieldIds ([], -1) fs
-      zfids = zip fids vs
-      zfs = zip fs vs
-      wits = map buildWitness zfids
-      imps = map buildFieldCodec zfs
-      maps = foldMap buildDefaultValue zfs
-      hmap = scalaNew "HMap[TFieldCodec]" True maps []
-      defs = scalaVal "defaults" True False Nothing [hmap]
-      maps' = map buildWitnessField zfs
-      hmap' = scalaNew "HMap[TFieldCodec]" True maps' []
-      lenc = scalaLambda [scalaArgument "x" Nothing Nothing] [hmap']
-      as = map buildAssignment zfs
-      fns = map (\(Field _ _ _ ident _) → ident) fs
-      c = scalaNew ident True (map scalaIdent fns) []
-      for = scalaFor as [c]
-      ldec = scalaLambda [scalaArgument "m" Nothing Nothing] [for]
-      menc = scalaMethod "encode" True [] Nothing [lenc]
-      mdec = scalaMethod "decode" True [] Nothing [ldec]
-      p = "TStructCodec[" ++ ident ++ "]"
-      in scalaCompanionObject ident (Just p) (wits ++ imps ++ [defs, menc, mdec])
-    buildDefaultValue (Field _ (Just Optional) ft _ cv, wid) = 
-      let
-        mv = maybe (scalaIdent "None") (\v → scalaNew "Some" True [buildValue v] []) cv
-        v = scalaPair 
-          (scalaField (scalaIdent ("w" ++ show wid)) "value" [] [])
-          mv
-      in [v]
-    buildDefaultValue (Field _ _ ft _ (Just cv), wid) = 
-      let
-        v = scalaPair 
-          (scalaField (scalaIdent ("w" ++ show wid)) "value" [] []) 
-          (buildValue cv)
-      in [v]
-    buildDefaultValue _ = []
-    buildWitnessField (Field _ _ _ ident _, wid) =
-      scalaPair (scalaField (scalaIdent $ "w" ++ show wid) "value" [] []) (scalaField (scalaIdent "x") ident [] [])
-    buildAssignment (Field _ _ _ ident _, wid) = let
-      w = scalaField (scalaIdent ("w" ++ show wid)) "value" [] []
-      f = scalaField (scalaIdent "m") "get" [w] []
-      f' = scalaField (scalaIdent "defaults") "get" [w] []
-      f'' = scalaField f "orElse" [f'] []
-      in scalaGenerator ident f''
+compile' (ThField _ fr ft ident fv) | Just Optional ← fr = [scalaArgument ident t1 v1]
+                                    | otherwise           = [scalaArgument ident t2 v2]
+  where t1 = Just $ "Option[" ++ buildType ft ++ "]"
+        v1 = Just $ maybe (scalaIdent "None") (scalaSome . buildValue) fv
+        t2 = Just $ buildType ft
+        v2 = maybe Nothing (Just . buildValue) fv
 
-buildUnion ∷ Identifier → [Field] → [Stmt]
-buildUnion ident fs = let
-  st = scalaSealedTrait ident (Just "TUnion") []
-  ccs = map buildCaseClass fs
-  vs = iterate (+1) 1
-  fids = fst $ foldr buildFieldIds ([], -1) fs
-  zfids = zip fids vs
-  zfs = zip fs vs
-  wits = map buildWitness zfids
-  imps = map buildFieldCodec zfs
-  menc = scalaMethod "encode" True [] Nothing $ map buildEncode zfs
-  ldec = scalaLambda [scalaArgument "m" Nothing Nothing] $ [foldr1 (\fa fb → scalaField fa "orElse" [fb] []) $ map buildDecode zfs]
-  mdec = scalaMethod "decode" True [] Nothing [ldec]
-  co = scalaCompanionObject ident (Just $ "TUnionCodec[" ++ ident ++ "]") (wits ++ imps ++ [menc, mdec])
-  in (st : ccs) ++ [co]
-  where
-    buildCaseClass (Field fid _ ft fn _) = 
-      scalaCaseClass (buildClassName fn) [scalaArgument fn (Just $ buildType ft) Nothing] [ident]
-    buildClassName (c : cs) = (toUpper c : cs)
-    buildEncode (Field _ _ _ fn _, wid) = let
-      n = scalaNew "HMap[TFieldCodec]" True [scalaPair (scalaField (scalaIdent $ "w" ++ show wid) "value" [] []) (scalaIdent "x")] []
-      in scalaCase ((buildClassName fn) ++ "(x)") Nothing [n]
-    buildDecode (Field _ _ _ ident _, wid) = let
-      f = scalaField (scalaIdent "m") "get" [scalaField (scalaIdent $ "w" ++ show wid) "value" [] []] []
-      n = scalaNew (buildClassName ident) True [scalaIdent "_"] []
-      in scalaField f "map" [n] []
+compile' _ = [scalaNoop]
+
+buildDocument ∷ [(Thrift, [Stmt])] → (Thrift, [Stmt]) → Stmt
+buildDocument is cu = scalaDocument p $ is' ++ snd cu
+  where (p, is') = foldr buildPackageAndImports (scalaPackage "default", []) is 
+
+buildPackageAndImports ∷ (Thrift, [Stmt]) → (Stmt, [Stmt]) → (Stmt, [Stmt])
+buildPackageAndImports i r@(p, is)
+  | Fix(ThNamespace NsJava _) ← fst i = (head $ snd i, is)
+  | Fix(ThNamespace NsStar _) ← fst i
+  , Fix(StPackage "default")  ← p     = (head $ snd i, is)
+  | Fix(ThInclude _)          ← fst i = (p, snd i ++ is)
+  | otherwise                          = r
 
 buildType ∷ FieldType → String
-buildType (FtBase BtBool) = "Boolean"
-buildType (FtBase BtByte) = "Byte"
-buildType (FtBase BtInt8) = "Byte"
-buildType (FtBase BtInt16) = "Short"
-buildType (FtBase BtInt32) = "Int"
-buildType (FtBase BtInt64) = "Long"
-buildType (FtBase BtDouble) = "Double"
-buildType (FtBase BtString) = "String"
-buildType (FtBase BtBinary) = "ByteVector"
-buildType (FtContainer (CtMap ftk ftv)) = "Map[" ++ (buildType ftk) ++ "," ++ (buildType ftv) ++ "]"
-buildType (FtContainer (CtSet ft)) = "Set[" ++ (buildType ft) ++ "]"
-buildType (FtContainer (CtList ft)) = "List[" ++ (buildType ft) ++ "]"
-buildType (FtNamed ident) = case (elemIndices '.' ident) of
-  [] → ident
-  is → drop (last is + 1) ident
+buildType FtBool = "Boolean"
+buildType FtByte = "Byte"
+buildType FtInt8 = "Byte"
+buildType FtInt16 = "Short"
+buildType FtInt32 = "Int"
+buildType FtInt64 = "Long"
+buildType FtDouble = "Double"
+buildType FtString = "String"
+buildType FtBinary = "ByteVector"
+buildType (FtMap ftk ftv) = "Map[" ++ buildType ftk ++ "," ++ buildType ftv ++ "]"
+buildType (FtSet ft) = "Set[" ++ buildType ft ++ "]"
+buildType (FtList ft) = "List[" ++ buildType ft ++ "]"
+buildType (FtNamed ident) = ident
 
 buildValue ∷ ConstValue → Stmt
 buildValue (CvInt i) = scalaLiteral i
@@ -209,44 +141,53 @@ buildValue (CvMap cs) = let
   cs'' = map (\(k,v) → scalaPair k v) $ zip cks cvs
   in scalaNew "Map" True cs'' []
 
-buildField ∷ Field → Stmt
-buildField (Field _ fr ft ident fv) = let
-  ft' = buildType ft
-  in case fr of
-    (Just Optional) → let 
-      fv' = maybe (scalaIdent "None") (\fv → scalaSome $ buildValue fv) fv
-      in scalaArgument ident (Just ("Option[" ++ buildType ft ++ "]")) (Just fv')
-    _ → let
-      fv' = maybe Nothing (\fv → Just $ buildValue fv) fv
-      in scalaArgument ident (Just $ buildType ft) fv'
+freshIds ∷ [Int]
+freshIds = iterate (+1) 1
 
-buildFieldIds ∷ Field → ([Int], Int) → ([Int], Int)
-buildFieldIds (Field (Just fid) _ _ _ _) (fids, fid') = (fid : fids, fid')
-buildFieldIds _ (fids, fid) = (fid : fids, fid - 1)
+buildWitness ∷ (Int, ThriftF Thrift) → Stmt
+buildWitness (wid, ThField (Just fid) _ _ _ _) =
+  let w = scalaNew "Witness" True [scalaLiteral fid] []
+  in scalaVal ("w" ++ show wid) False False Nothing [w]
 
-buildWitness ∷ (Int, Int) → Stmt
-buildWitness (fid, var) = let
-  wit = scalaNew "Witness" True [scalaLiteral fid] []
-  in scalaVal ("w" ++ show var) False False Nothing [wit]
+buildCodec ∷ (Int, ThriftF Thrift) → Stmt
+buildCodec (wid, ThField _ fr ft _ _) = scalaVal ("r" ++ show wid) False True Nothing [fc]
+  where fc = scalaNew fn False [] []
+        fn = "TFieldCodec[w" ++ show wid ++ ".T," ++ ft'' ++ "]"
+        ft' = buildType ft
+        ft'' | Just Optional ← fr = "Option[" ++ ft' ++ "]"
+             | otherwise           = ft''
 
-buildFieldCodec ∷ (Field, Int) → Stmt
-buildFieldCodec (Field _ fr ft _ _, var) = let
-  ft' = buildType ft
-  ft'' = case fr of
-    Just Optional → "Option[" ++ ft' ++ "]"
-    _ → ft'
-  fn = "TFieldCodec[w" ++ (show var) ++ ".T," ++ ft'' ++ "]"
-  fc = scalaNew fn False [] []
-  in scalaVal ("r" ++ show var) False True Nothing [fc]
+buildDefaultValue ∷ (Int, ThriftF Thrift) → [Stmt]
+buildDefaultValue (wid, ThField _ fr _ _ cv) | Just Optional ← fr = [scalaPair k v]
+                                             | Just cv'      ← cv = [scalaPair k $ buildValue cv']
+                                             | otherwise          = []
+  where k = scalaField i "value" [] []
+        i = scalaIdent $ "w" ++ show wid
+        v = maybe (scalaIdent "None") (\v → scalaNew "Some" True [buildValue v] []) cv
+        
+buildPair ∷ (Int, ThriftF Thrift) → Stmt
+buildPair (wid, ThField _ _ _ ident _) = scalaPair k v
+  where k = witnessValue wid
+        v = scalaField (scalaIdent "x") ident [] []
 
-buildException ∷ Identifier → [Field] → Map.Map FilePath [Stmt]
-buildException ident fs = Map.empty
+buildFor ∷ (Int, ThriftF Thrift) → Stmt
+buildFor (wid, ThField _ _ _ ident _) = scalaGenerator ident r
+  where r = scalaField r' "orElse" [r''] []
+        r' = scalaField (scalaIdent "m") "get" [w] []
+        r'' = scalaField (scalaIdent "defaults") "get" [w] []
+        w = witnessValue wid
 
-buildConst ∷ Definition → [Stmt] → [Stmt]
-buildConst (Const ft ident cv) ss = (scalaVal ident False False (Just $ buildType ft) []) : ss
-buildConst _ ss = ss
+buildCase ∷ (Int, ThriftF Thrift) → Stmt
+buildCase (wid, ThField _ _ _ ident _) = scalaCase (ident ++ "(x)") Nothing [n]
+  where n = scalaNew "HMap[TFieldCodec]" True [scalaPair (witnessValue wid) $ scalaIdent "x"] []
 
-buildPackageObjectName ∷ String → String
-buildPackageObjectName pkg = case (elemIndices '.' pkg) of
-  [] → pkg
-  is → drop ((last is) + 1) pkg
+buildGet ∷ (Int, ThriftF Thrift) → Stmt
+buildGet (wid, ThField _ _ _ ident _) = scalaField f "map" [n] []
+  where f = scalaField (scalaIdent "m") "get" [witnessValue wid] []
+        n = scalaNew ident True [scalaIdent "_"] []
+
+getIdent ∷ (Int, ThriftF Thrift) → String
+getIdent (_, ThField _ _ _ ident _) = ident
+
+witnessValue ∷ Int → Stmt
+witnessValue i = scalaField (scalaIdent $ "w" ++ show i) "value" [] []
